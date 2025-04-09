011e7da91a8923ccd8f905ac6683ccc02a3fcb98
Gabriel <93326437+GabrielMarengoQM@users.noreply.github.com>
2025-04-09 10:55:10 +0100

    Delete deploy_app.R

--- deploy_app.R
+++
@@ -1,8 +0,0 @@
-# Push to shinyapp.io
-library(rsconnect)
-
-rsconnect::deployApp(
-  appFiles = c("app.R", "mydb.duckdb", './modules/goORA_module.R', './modules/reactomeORA_module.R', './modules/main_module.R', './modules/oddsRatio_module.R'),
-  appName = "EGEx_dev"
-)
-
